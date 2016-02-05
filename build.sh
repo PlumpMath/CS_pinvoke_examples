f91527f70f68cd5c38f4d881d0f4ee5a5255c2df
pius.lee <pius.lee@samsung.com>
2016-02-05 17:53:23 +0900

    fix build script

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ gcc -ggdb -Wall -Werror -fpic -I . -rdynamic -shared test.c -o libtest.so
 gcc -I. main.c -L. -ltest
 
 mcs /t:library test_plat.cs
-mcs /t:library test_oo.cs
+#mcs /t:library test_oo.cs
 
 mcs /reference:test_plat.dll /t:exe main_plat.cs
-mcs /reference:test_oo.dll /t:exe main_oo.cs
+#mcs /reference:test_oo.dll /t:exe main_oo.cs
